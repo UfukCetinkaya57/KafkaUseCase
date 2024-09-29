4da6729d972d8d9462537f98f50497699b8e4ed2
Ufuk <Ufuk@DESKTOP-8T9IQ2C>
2024-09-30 02:32:16 +0300

    Add project files.

---
+++ Shared.Events/CustomValueSerializer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+
+namespace Shared.Events
+{
+    public class CustomValueSerializer<T> : ISerializer<T>
+    {
+        public byte[] Serialize(T data, SerializationContext context)
+        {
+            return Encoding.UTF8.GetBytes(JsonSerializer.Serialize(data, typeof(T)));
+        }
+    }
+}
